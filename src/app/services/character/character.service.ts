80d4ec8ea3e89e5444f8e887fc68020aa657be97
Jarpos <jarpos@protonmail.com>
2020-12-23 18:28:08 +0100

    add service

--- src/app/services/character/character.service.ts
+++ src/app/services/character/character.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { CharInfo_c } from "./types/CharInfo";
+import { BaseValue_c, get_base_values } from "./types/BaseValue";
+import { Talent_c, get_talents } from "./types/Talent";
+import { Character_c } from './types/Character';
+
 @Injectable({
     providedIn: 'root'
 })
 export class CharacterService {
 
+    public _characters: Character_c[] = [
+        new Character_c(),
+        new Character_c(),
+        new Character_c(),
+    ];
+
     constructor() { }
+
 }
