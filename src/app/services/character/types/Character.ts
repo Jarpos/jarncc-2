fd9122b85a456c1905d7e259c32e628725ced719
Jarpos <jarpos@protonmail.com>
2020-12-23 17:34:06 +0100

    use updated CharInfo

--- src/app/services/character/types/Character.ts
+++ src/app/services/character/types/Character.ts
@@ -1,16 +1,11 @@
-import { CharInfo_c } from "./CharInfo";
+import { CharInfo_c, get_char_infos } from "./CharInfo";
 import { BaseValue_c, get_base_values } from "./BaseValue";
 import { Talent_c, get_talents } from "./Talent";
 
 export class Character_c {
-    /**
-     * @param c character information
-     */
-    constructor(c: CharInfo_c) {
-        this._information = c;
-    }
+    constructor() { }
 
-    public _information: CharInfo_c = new CharInfo_c();
+    public _information: Map<string, CharInfo_c<number | string>> = get_char_infos();
     public _baseValues: Map<string, BaseValue_c> = get_base_values();
     public _talents: Map<string, Talent_c> = get_talents();
 }
