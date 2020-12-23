6686b2afbd9f5a4ead2d1d103d48f9cc95da1d0d
Jarpos <jarpos@protonmail.com>
2020-12-23 18:42:47 +0100

    add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CCharEditorComponent } from './pages/c-char-editor/c-char-editor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -16,6 +18,8 @@ import { CCharEditorComponent } from './pages/c-char-editor/c-char-editor.compon
         AppRoutingModule,
         FormsModule,
         ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
