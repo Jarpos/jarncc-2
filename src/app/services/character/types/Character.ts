592d78d6790b37219b1925248eb8a066e63588d1
Jarpos <jarpos@protonmail.com>
2020-12-27 02:06:57 +0100

    add getters for calculated stats

--- src/app/services/character/types/Character.ts
+++ src/app/services/character/types/Character.ts
@@ -1,5 +1,5 @@
 import { CharInfo_c, get_char_infos } from "./CharInfo";
-import { BaseValue_c, get_base_values } from "./BaseValue";
+import { BaseValue_c, get_base_values, get_base_values_index } from "./BaseValue";
 import { Talent_c, get_talents } from "./Talent";
 import { Proficency_c } from "./Proficency";
 
@@ -28,4 +28,40 @@ export class Character_c {
 
     public get talentPoints(): number { return this._proficency.TalentPoints; }
     public get maxTalentPoints(): number { return this._proficency.MaxTalentPoints; }
+
+
+    private calculate_from_base(TA: number, FL: number, FF: number, IN: number, GH: number, KR: number): number {
+        var map = get_base_values_index();
+        return TA * this._baseValues[map.get("TA")!].value +
+               FL * this._baseValues[map.get("FL")!].value +
+               FF * this._baseValues[map.get("FF")!].value +
+               IN * this._baseValues[map.get("IN")!].value +
+               GH * this._baseValues[map.get("GH")!].value +
+               KR * this._baseValues[map.get("KR")!].value;
+    }
+
+    public get health(): number {
+        // NOTE: Origin of Character is not taken into account
+        return this.calculate_from_base(0.0, 1.0, 0.0, 0.0, 2.0, 1.0);
+    }
+
+    public get initiative(): number {
+        return this.calculate_from_base(1.0, 1.0, 0.5, 0.0, 0.0, 0.0);
+    }
+
+    public get dodge(): number {
+        return this.calculate_from_base(0.5, 1.5, 1.0, 1.0, 0.5, 0.5);
+    }
+
+    public get dodge_cap(): number {
+        return this.calculate_from_base(1.0, 2.0, 1.5, 1.5, 1.0, 1.0);
+    }
+
+    public get parry(): number {
+        return this.calculate_from_base(1.5, 1.0, 0.5, 0.5, 0.5, 1.0);
+    }
+
+    public get parry_cap(): number {
+        return this.calculate_from_base(2.0, 1.5, 1.0, 1.0, 1.0, 1.5);
+    }
 }
