8bc8f13acfe040a335ada6343e257ac6384bcf65
Jarpos <jarpos@protonmail.com>
2020-12-27 16:37:26 +0100

    add import/export button WIP

--- src/app/pages/c-char-editor/c-char-editor.component.ts
+++ src/app/pages/c-char-editor/c-char-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CharacterService } from 'src/app/services/character/character.service';
+import { Character_c } from 'src/app/services/character/types/Character';
 
 @Component({
     selector: 'app-c-char-editor',
@@ -22,5 +23,11 @@ export class CCharEditorComponent implements OnInit {
     public log() {
         console.log(this.characterService._character);
     }
+
+
+    // Does not work yet
+    public textArea: string = "";
+    public export() { this.textArea = JSON.stringify(this.character); }
+    public import() { this.character = (JSON.parse(this.textArea) as Character_c); }
 
 }
