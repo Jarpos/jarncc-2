ea441b238ecfe2cedb0e53aeb5362ec3c14033f6
Jarpos <jarpos@protonmail.com>
2020-12-29 03:08:06 +0100

    add weapon talents, update constructor & change difficulty

--- src/app/services/character/types/CalculatedTalents.ts
+++ src/app/services/character/types/CalculatedTalents.ts
@@ -15,7 +15,7 @@ export class CalculatedTalents_c {
      * @param _this reference to parent object
      * @param expl explanation string
      */
-    constructor(n: string, base: Factors_c, cap: Factors_c, _this: Character_c, expl: string) {
+    constructor(n: string, base: Factors_c, cap: Factors_c | null, _this: Character_c, expl: string) {
         this.name = n;
         this.explanation = expl,
         this.parent = _this;
@@ -27,7 +27,7 @@ export class CalculatedTalents_c {
     public name: string = "";
     public explanation: string = "";
 
-    private _difficulty: difficulties = 1;
+    private _difficulty: difficulties = 2;
     public get difficulty(): string {
         // 65 is A, so we use +64 to force at least a 65
         return String.fromCharCode(this._difficulty + 64);
@@ -37,13 +37,17 @@ export class CalculatedTalents_c {
 
     // Factors that the base is multiplied by
     private _base: Factors_c;
-    private _cap: Factors_c;
+    private _cap: Factors_c | null;
 
     public get basePoints(): number {
         return calc_from_base(this._base, this.parent._baseValues);
     }
     public get capPoints(): number {
-        return calc_from_base(this._cap, this.parent._baseValues);
+        if (this._cap === null) {
+            return 100;
+        } else {
+            return calc_from_base(this._cap, this.parent._baseValues);
+        }
     }
 
     private _extraPoints: number = 0;
@@ -63,12 +67,37 @@ export class CalculatedTalents_c {
 
 
 export function get_calculated_talents(_this: Character_c): CalculatedTalents_c[] {
+    const weapon_talent =
+        (KR: number, GH: number, FL: number, FF: number) => {
+            return new Factors_c(0.0, FL, FF, 0.0, GH, KR);
+        };
+
     return [
         new CalculatedTalents_c("Ausweichen", new Factors_c(0.5, 1.5, 1.0, 1.0, 0.5, 0.5),
                                               new Factors_c(1.0, 2.0, 1.5, 1.5, 1.0, 1.0),
                                               _this, "Wie gut man im Kampf ausweichen kann"),
-        new CalculatedTalents_c("Parieren", new Factors_c(1.5, 1.0, 0.5, 0.5, 0.5, 1.0),
-                                            new Factors_c(2.0, 1.5, 1.0, 1.0, 1.0, 1.5),
-                                            _this, "Wie gut man parieren kann"),
+        new CalculatedTalents_c("Parieren",   new Factors_c(1.5, 1.0, 0.5, 0.5, 0.5, 1.0),
+                                              new Factors_c(2.0, 1.5, 1.0, 1.0, 1.0, 1.5),
+                                              _this, "Wie gut man parieren kann"),
+
+        // One-handed-weapons
+        new CalculatedTalents_c("Hiebwaffen",   weapon_talent(1.0, 0.5, 1.5, 1.0),
+                                                null, _this, "Einhändige Hiebwaffe"),
+        new CalculatedTalents_c("Stichwaffen",  weapon_talent(1.0, 1.0, 1.5, 0.5),
+                                                null, _this, "Einhändige Stichwaffe"),
+        new CalculatedTalents_c("Wurfwaffen",   weapon_talent(0.0, 1.0, 0.5, 1.5),
+                                                null, _this, "Einhändige Wurfwaffen"),
+        new CalculatedTalents_c("Kettenwaffen", weapon_talent(1.0, 1.0, 1.5, 0.5),
+                                                null, _this, "Einhändige Kettenwaffen"),
+
+        // Two-handed-weapons
+        new CalculatedTalents_c("Hiebwaffen",    weapon_talent(2.0, 1.0, 1.0, 0.5),
+                                                 null, _this, "Zweihändige Hiebwaffen"),
+        new CalculatedTalents_c("Stangenwaffen", weapon_talent(2.0, 1.0, 1.0, 0.5),
+                                                 null, _this, "Zweihändige Stangenwaffen"),
+        new CalculatedTalents_c("Bogen",         weapon_talent(0.0, 1.0, 1.0, 1.5),
+                                                 null, _this, "Zweihändiger Bogen"),
+        new CalculatedTalents_c("Armbrüste",     weapon_talent(1.0, 0.5, 1.0, 1.5),
+                                                 null, _this, "Zweihändige Armbrust"),
     ];
 }
